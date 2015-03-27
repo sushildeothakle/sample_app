056087f0d3bced2fff92435514937e8d8ef00ed5
Sushil Deothakle <sushildeothakle@gmail.com>
2015-03-27 12:02:07 +0530

    Change

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+root 'movies#user'
 get '/user'=> 'movies#user'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
