f1e3b5d08ac6def3c3036f350a1663e1ab2818bd
Sushil Deothakle <sushildeothakle@gmail.com>
2015-03-31 16:04:16 +0530

    user model validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,13 @@ def new
 	@title="sign up"
 	
 end
+def create()
+	User.create(user_params)
+	
+end
+
+private
+def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,8 @@
 #
 
 class User < ActiveRecord::Base
-attr_accessible :name, :email
+# attr_accessible :name, :email
+
+validates :name ,:presence => true, :length => { :maximum => 50 }
+validates :email ,:presence => true, :uniqueness => true
 end
