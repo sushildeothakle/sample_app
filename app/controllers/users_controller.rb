245be1acef1ec32e06665f52b3db1878a67183f5
Sushil Deothakle <sushildeothakle@gmail.com>
2015-04-01 14:55:31 +0530

    added password & encrypted password in user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,6 @@ end
 
 private
 def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,19 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string
-#  email      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                 :integer          not null, primary key
+#  name               :string
+#  email              :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  encrypted_password :string
 #
 
 class User < ActiveRecord::Base
 # attr_accessible :name, :email
+attr_accessor :password 
 
-validates :name ,:presence => true, :length => { :maximum => 50 }
-validates :email ,:presence => true, :uniqueness => true
+validates :name , :presence => true, :length => { :maximum => 50 }
+validates :email , :presence => true, :uniqueness => true
+validates :password, :presence => true, :confirmation => true
 end
