79e27e3d6ec7fb3bbe78a7544ed637c03995fd4d
Sushil Deothakle <sushildeothakle@gmail.com>
2015-04-01 16:35:03 +0530

    password machinary

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401092008) do
+ActiveRecord::Schema.define(version: 20150401103226) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150401092008) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
